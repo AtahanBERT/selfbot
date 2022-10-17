198bc6d907b191da646d1f19ca412d82c7190d2c
Glitch (atahan-selfbot) <none>
2022-10-17 16:37:52 +0000

    🐜🐂 Checkpoint ./komutlar/etiket-değiş.js:26490528/171 ./komutlar/isim-değiş.js:26490528/9

--- komutlar/etiket-değiş.js
+++ komutlar/etiket-değiş.js
@@ -8,16 +8,17 @@ exports.run = async (client, message, args) => {
 if (message.author.id === ayarlar.sahip) {
 
 if (client.user.nitroType !== "NITRO_BOOST" & client.user.nitroType !== "NITRO_CLASSIC") return message.reply(`${basarisiz} ${message.author}, Bu komut için \`NITRO CLASSIC\` veya \`NITRO BOOST\` a ihtiyacın var.`).then(x => setTimeout(() => {x.delete()}, 5000))
-if (!client.password || !process.env.password) return message.reply({content:`${basarisiz} ${message.author}, Lütfen projeye girip şifrenizi belirtiniz.`}).then(x => setTimeout(() => {x.delete()}, 5000))
+if (!client.password & !process.env.password) return message.reply({content:`${basarisiz} ${message.author}, Lütfen projeye girip şifrenizi belirtiniz.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 if (isNaN(args[0])) return message.reply({content:`${basarisiz} ${message.author}, Yanlış kullanım doğrusu -> ${ayarlar.prefix}etiket <etiket> şeklinde yazınız.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 if (args[0] === client.user.discriminator) return message.reply({content:`${basarisiz} ${message.author}, Belirtilen etiket önceki ile aynı olamaz.`}).then(x => setTimeout(() => {x.delete()}, 5000))
+if (args[0] > 4 || args[0] <)
   
 try {
- client.user.setDiscriminator(args[0], client.password || process.env.password).then(() => {
+ await client.user.setDiscriminator(args[0], client.password || process.env.password).then(() => {
   message.reply({content:`${basari} ${message.author}, Başarıyla yeni etiketin **#${args[0]}** olarak ayarlandı.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 message.react('✅')
 })
-} catch {message.reply({content:`${basarisiz} ${message.author}, Etiketini çok hızlı değişiyosun 1 saat sonra tekrar dene.`}).then(x => setTimeout(() => {x.delete()}, 5000))
+} catch {message.reply({content:`${basarisiz} ${message.author}, Bu etiket baskaşı tarafından alınmış veya etiketini çok hızlı değişiyosun 1 saat sonra tekrar dene.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 }
 }
 };
--- komutlar/isim-değiş.js
+++ komutlar/isim-değiş.js
@@ -7,12 +7,12 @@ exports.run = async (client, message, args) => {
   
 if (message.author.id === ayarlar.sahip) {
 
-if (!client.password || !process.env.password) return message.reply({content:`${basarisiz} ${message.author}, Lütfen projeye girip şifrenizi belirtiniz.`}).then(x => setTimeout(() => {x.delete()}, 5000))
+if (!client.password & !process.env.password) return message.reply({content:`${basarisiz} ${message.author}, Lütfen projeye girip şifrenizi belirtiniz.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 if (!args.join(" ")) return message.reply({content:`${basarisiz} ${message.author}, Yanlış kullanım doğrusu -> ${ayarlar.prefix}isim <isim> şeklinde yazınız.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 if (args.join(" ") === client.user.username) return message.reply({content:`${basarisiz} ${message.author}, Belirtilen isim önceki ile aynı olamaz.`}).then(x => setTimeout(() => {x.delete()}, 5000))
   
 try {
- client.user.setUsername(args.join(" "), client.password || process.env.password).then(() => {
+ await client.user.setUsername(args.join(" "), client.password || process.env.password).then(() => {
   message.reply({content:`${basari} ${message.author}, Başarıyla yeni ismin **${args.join(" ")}** olarak ayarlandı.`}).then(x => setTimeout(() => {x.delete()}, 5000))
 message.react('✅')
 })
